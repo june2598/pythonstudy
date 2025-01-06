260899e00ed8faba7c9b4e323b953ef1f5e6deaa
junwookim <hydrlisk30@gmail.com>
2025-01-06 17:32:32 +0900

    20250106

--- project/data_loader.py
+++ project/data_loader.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def load_sector_data(market):
+def load_sector_data(market=None, month_ago=1):
     """
     주어진 시장에 따라 적절한 CSV 파일을 로드하는 함수.
 
@@ -13,14 +13,32 @@ def load_sector_data(market):
     Raises:
     ValueError: 지원되지 않는 시장이 입력된 경우.
     """
-    if market == 'KOSPI':
-        df = pd.read_csv('new_KOSPI_add_sector_df.csv')
-    elif market == 'KOSDAQ':
-        df = pd.read_csv('new_KOSDAQ_add_sector_df.csv')
-    elif market == 'ETF':
-        df = pd.read_csv('new_ETF_add_sector_df.csv')    
+    if month_ago < 0:
+        raise ValueError("month_ago는 0 이상의 정수여야 합니다.")
+    
+    if market in ['KOSPI', 'KOSDAQ', 'ETF'] :
+        print(f'{month_ago}개월 간의 data를 불러옵니다.')
+        df = pd.read_csv(f'{market}_add_sector_{month_ago}m.csv')
     else:
         raise ValueError(" 'KOSPI', 'KOSDAQ', 'ETF' 세 시장만 지원하는 기능입니다.")
       
     return df  
     
+    
+def get_market_color(market):
+    """
+    주어진 시장에 따른 색상을 반환하는 함수.
+
+    Parameters:
+    market (str): 'KOSPI', 'KOSDAQ', 또는 'ETF' 중 하나를 선택.
+
+    Returns:
+    str: 해당 시장에 대한 색상 코드.
+    """
+    market_colors = {
+        'KOSPI': 'skyblue',
+        'KOSDAQ': 'red',
+        'ETF': 'lightgreen'
+    }
+    
+    return market_colors.get(market, 'gray')
